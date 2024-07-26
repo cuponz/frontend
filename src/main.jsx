512384a90b551da8abdaaf18cb49dbba7e681ef9
eli_0903 <91484965+Walts03@users.noreply.github.com>
2024-07-26 21:46:53 +1000

    1st commit: frontend structure + router
    Setup folder and router for frontend

---
+++ src/main.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
+import ContactUs from "./pages/ContactUs.jsx"
+import "./index.css";
+import ErrorPage from "./pages/ErrorPage.jsx";
+import AboutUs from "./pages/AboutUs.jsx";
+const router = createBrowserRouter([
+  {
+  path:'/',
+  element: <HomePage />,
+  errorElement: <ErrorPage/>
+  },
+  {
+    path:'/contactus',
+    element: <ContactUs />
+  },
+  {
+    path:'/aboutus',
+    element: <AboutUs />
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>
+);
---
+++ src/pages/ContactUs.jsx
@@ -0,0 +1,4 @@
+function App() {
+  return <h1>Contact Us</h1>;
+}
+export default App;
