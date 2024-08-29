ff25e3130a6b4761050e5747d03fa223c27df213
haram123 <haram.chaudhary123@gmail.com>
2024-08-29 21:35:30 +1000

    Added Customer Profile
    Added customer profile and changed Nav bar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,32 +1,42 @@
-import React from 'react';
-import './navbar.css';
+import React from "react";
+import "./navbar.css";
+import { menuItems } from "../data/menuData.js";
 
 const Navbar = () => {
   return (
     <div className="navbar-background">
       <nav className="navbar">
         <div className="navbar-left">
-          <a href="/">Home</a>
-          <a href="#">Hot Deals</a>
-          <div className="dropdown">
-            <button className="dropbtn">Categories
-              <i className="arrow down"></i>
-            </button>
-            <div className="dropdown-content">
-              <a href="#">Desktop</a>
-              <a href="#">Laptop</a>
-              <a href="#">Mobile</a>
-              <a href="#">Xiaomi</a>
-              <a href="#">Huawei</a>
-              {/* Add more categories as needed */}
-            </div>
-          </div>
-          <a href="contactus">Contact Us</a>
-          <a href="aboutus">About Us</a>
+          {menuItems.map((item, index) => {
+            if (item.dropdown) {
+              return (
+                <div className="dropdown" key={index}>
+                  <button className="dropbtn">
+                    {item.name}
+                    <i className="arrow down"></i>
+                  </button>
+                  <div className="dropdown-content">
+                    {item.items.map((subItem, subIndex) => (
+                      <a href={subItem.link} key={subIndex}>
+                        {subItem.name}
+                      </a>
+                    ))}
+                  </div>
+                </div>
+              );
+            } else {
+              return (
+                <a href={item.link} key={index}>
+                  {item.name}
+                </a>
+              );
+            }
+          })}
         </div>
         <div className="navbar-right">
           <button className="search-btn">
-            <i className="fa fa-search"></i> {/* Using FontAwesome for the search icon */}
+            <i className="fa fa-search"></i>{" "}
+            {/* Using FontAwesome for the search icon */}
           </button>
           <button className="login-btn">Login</button>
         </div>
---
+++ src/data/demo_cust_prof.js
@@ -0,0 +1,8 @@
+[
+  {
+    id: "1",
+    name: "My Name",
+    email: "mymailorphone@gmail.com",
+    profileImageUrl: "path-to-image.jpg", // Provide the actual path or URL to the image
+  },
+];
