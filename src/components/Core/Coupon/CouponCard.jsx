499d141b5527293b0460d6abcda39cbf58b85c7d
newbee1905 <beenewminh@outlook.com>
2024-10-05 05:09:41 +1000

    feat(wip): temporary adding coupon code to coupon card, may need to be redesigned later

--- src/components/Core/Coupon/CouponCard.jsx
+++ src/components/Core/Coupon/CouponCard.jsx
@@ -1,10 +1,19 @@
-import { CouponCardModalType, CouponCatalougeType, RedemptionState } from "../../../constants";
+import {
+  CouponCardModalType,
+  CouponCatalougeType,
+  RedemptionState,
+} from "../../../constants";
 import { useState } from "react";
 
 import CouponPopup from "./CouponPopup";
 import InfoField from "./InfoField";
 
-const CouponCardActionButton = ({ type, onAction, isRedeemed = false, isUsed = false }) => {
+const CouponCardActionButton = ({
+  type,
+  onAction,
+  isRedeemed = false,
+  isUsed = false,
+}) => {
   let text;
 
   switch (type) {
@@ -25,26 +34,24 @@ const CouponCardActionButton = ({ type, onAction, isRedeemed = false, isUsed = f
       <button
         onClick={onAction}
         disabled={isUsed || isRedeemed}
-        className={`px-4 py-2 rounded-md w-full text-white ${isUsed || isRedeemed
+        className={`px-4 py-2 rounded-md w-full text-white ${
+          isUsed || isRedeemed
             ? "bg-gray-400 cursor-not-allowed"
             : "bg-[#46467A] hover:bg-green-700"
-          }`}
+        }`}
       >
         {text}
       </button>
     </div>
-  )
-}
+  );
+};
 
-const CouponCard = ({
-  coupon,
-  type,
-  onShowStats,
-}) => {
+const CouponCard = ({ coupon, type, onShowStats }) => {
   const [modalType, setModalType] = useState(null);
 
   let {
     id,
+    code,
     logo_url: logo,
     title,
     desc: description,
@@ -62,8 +69,8 @@ const CouponCard = ({
 
   keywords = keywords.split(",");
 
-  startDate = (new Date(startDate)).toLocaleDateString();
-  endDate = (new Date(endDate)).toLocaleDateString();
+  startDate = new Date(startDate).toLocaleDateString();
+  endDate = new Date(endDate).toLocaleDateString();
 
   const handleClose = () => {
     setModalType(null);
@@ -90,22 +97,31 @@ const CouponCard = ({
 
   return (
     <div className="bg-white shadow-md rounded-lg p-4 flex flex-col justify-between h-full relative">
-      {(type === CouponCatalougeType.All || type === CouponCatalougeType.ShopList) && maxUsage && (
-        <div className="absolute top-0 right-0 bg-yellow-500 text-black text-xs font-bold px-2 py-1 rounded-bl-lg">
-          {maxUsage - usageCount} coupons left!
-        </div>
-      )}
+      {(type === CouponCatalougeType.All ||
+        type === CouponCatalougeType.ShopList) &&
+        maxUsage && (
+          <div className="absolute top-0 right-0 bg-yellow-500 text-black text-xs font-bold px-2 py-1 rounded-bl-lg">
+            {maxUsage - usageCount} coupons left!
+          </div>
+        )}
 
-      {type === CouponCatalougeType.ShopManage &&
+      {type === CouponCatalougeType.ShopManage && (
         <div className="absolute top-0 right-0 bg-yellow-500 text-black text-xs font-bold px-2 py-1 rounded-bl-lg">
           {numUsers} Users
         </div>
-      }
+      )}
 
       <div className="flex flex-col items-center text-center mb-4 flex-grow">
         <div className="mb-4">
-          <img src={logo} alt={title} className="sm:w-48 md:w-96 h-96 object-contain" />
+          <img
+            src={logo}
+            alt={title}
+            className="sm:w-48 md:w-96 h-96 object-contain"
+          />
         </div>
+        <h1 className="text-3xl font-semibold font-mono mb-3 transition-all tracking-wide hover:tracking-widest hover:text-[#46467A]">
+          {code}
+        </h1>
         <h2 className="text-lg font-semibold mb-1">{title}</h2>
         <p className="text-gray-700 text-sm mb-2">{shop}</p>
         <p className="mt-2 text-gray-600 text-sm">{categoryName}</p>
