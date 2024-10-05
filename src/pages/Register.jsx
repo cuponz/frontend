15b4d28669cddf4e5e0ea9c68d004de39a9d1b1c
newbee1905 <beenewminh@outlook.com>
2024-10-05 21:21:00 +1000

    fix: fix typo for settings

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,7 +4,7 @@ import CouponImage2 from "../assets/coupon2.png";
 import { toast } from "sonner";
 
 import { Link, useNavigate } from "react-router-dom";
-import Layout from "../layout/Layout";
+import Layout from "@/layout/Layout";
 import { UserType, CountryListWithCode } from "../constants";
 import { useMutation } from "@tanstack/react-query";
 import { userRegister } from "../api/user";
